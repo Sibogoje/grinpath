8237ec1671dd1232a91a57a9ccd1de30889e45f9
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-06 06:10:52 +0200

    refactor: move header and sidebar markup to header.php for better code organization in index.php

--- index.php
+++ index.php
@@ -90,21 +90,8 @@
     </style>
 </head>
 <body onclick="hideSidebar()">
-    <div class="header" onclick="event.stopPropagation()">
-        <span class="menu-icon" onclick="toggleSidebar()">&#9776;</span>
-        <h6>Public Transport Incident Report</h6>
-        <img src="bika logo.png" alt="Transport Logo" class="logo">
-    </div>
-    
-    <div id="sidebar" class="sidebar" onclick="event.stopPropagation()">
-        <span class="close-btn" onclick="hideSidebar()">&#10005;</span>
-        <div class="sidebar-logo">
-            <img src="bika logo.png" alt="Transport Logo">
-        </div>
-        <a href="index.php">Home</a>
-        <a href="#">Partners</a>
-        <a href="#">About</a>
-    </div>
+   
+<?php include 'header.php';
     
     <div class="container" onclick="event.stopPropagation()">
         <p class="text-center">Help us improve transport services by reporting incidents.</p>
