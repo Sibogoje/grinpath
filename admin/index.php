ba76c18f0ce9fb203442601b568ac59644860956
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-09 11:11:12 +0200

    refactor: remove redundant admin dashboard markup for cleaner layout

--- admin/index.php
+++ admin/index.php
@@ -8,15 +8,5 @@
 </head>
 <body>
     <?php include 'header.php'; ?>
-    <div class="container">
-        <h1>Admin Dashboard</h1>
-        <ul>
-            <li><a href="dashboard.php">Dashboard</a></li>
-            <li><a href="users.php">Users</a></li>
-            <li><a href="manage_partners.php">Manage Partners</a></li>
-            <li><a href="manage_vehicles.php">Manage Vehicles</a></li>
-            <li><a href="customer_reports.php">Customer Reports</a></li>
-        </ul>
-    </div>
 </body>
 </html>
